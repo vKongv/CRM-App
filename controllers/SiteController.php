800114fc7ca969d862090fe12ba0835ecbf03b02
vKongv <kaweng456@hotmail.com>
2016-07-27 15:46:26 +0800

    Test commit on feature branch

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,7 +3,7 @@ namespace app\controllers;
 use \yii\web\Controller;
 class SiteController extends Controller{
   public function actionIndex(){
-    return 'Our CRM';
+    return 'Our CRM Test add feature';
   }
 
   public function actionTest(){
